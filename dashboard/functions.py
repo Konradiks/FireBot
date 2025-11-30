38fed6b2058c8bcdfbab4ee7f3a3d884afaed9e5
Konrad <konradrafal.bachor@gmail.com>
2025-11-30 14:08:40 +0100

    rule command view visual changes

--- dashboard/functions.py
+++ dashboard/functions.py
@@ -65,12 +65,15 @@ def get_command_html(command):
     </head>
     <body>
 
-        <div style="display: flex; flex-direction: column">
+        <div style="display: flex; flex-direction: column; width:800px;">
             <h3>Wygenerowana komenda:</h3>
-            <div id="apiSection">
-                <label>Wpisz klucz API:</label><br>
-                <input id="apiInput" type="text" placeholder="API key" style="width:300px; padding:5px;">
-                <button onclick="updateApiKey()">Uzupełnij klucz</button>
+            <div style="display: flex; align-items: end;">
+                <div id="apiSection">
+                    <label>Wpisz klucz API:</label><br>
+                    <input id="apiInput" type="text" placeholder="API key" style="width:300px; padding:5px;">
+                    <button onclick="updateApiKey()">Uzupełnij klucz</button>
+                </div>
+                <a href="https://docs.paloaltonetworks.com/pan-os/11-0/pan-os-panorama-api/get-started-with-the-pan-os-xml-api/get-your-api-key" target="_blank">Jak pozyskać klucz api</a>
             </div>
         </div>
 
@@ -113,32 +116,61 @@ def get_command_html(command):
 
 
 
+# def gen_panos_securityrules_unblock(host: str, ip_list: list, vsys: str = "vsys1", api_key: str = "<YOUR-API-KEY>"):
+#     """
+#     Generate a single curl command (or multiline command) that deletes FireBot deny rules for given IPs.
+#
+#     Args:
+#         host: firewall IP/hostname
+#         api_key: PAN-OS API key
+#         ip_list: list of IPs to unblock
+#         vsys: VSYS name (default: vsys1)
+#
+#     Returns:
+#         str: curl command(s) for deleting rules
+#     """
+#
+#     cmd_command = ""
+#
+#     for ip in ip_list:
+#         rule_name = f"FireBot-{ip}"
+#         url = f"{host}/restapi/v11.0/Policies/SecurityRules?location=vsys&vsys={vsys}&name={rule_name}"
+#
+#         cmd_command += "curl -k -X DELETE \\\n"
+#         cmd_command += "+  '" + url + "' \\\n"
+#         cmd_command += "+  -H 'X-PAN-KEY: " + api_key + "' \\\n"
+#         cmd_command += "+  -H 'Content-Type: application/json'\n\n"
+#
+#     return cmd_command
+
 def gen_panos_securityrules_unblock(host: str, ip_list: list, vsys: str = "vsys1", api_key: str = "<YOUR-API-KEY>"):
     """
-    Generate a single curl command (or multiline command) that deletes FireBot deny rules for given IPs.
-
-    Args:
-        host: firewall IP/hostname
-        api_key: PAN-OS API key
-        ip_list: list of IPs to unblock
-        vsys: VSYS name (default: vsys1)
-
-    Returns:
-        str: curl command(s) for deleting rules
+    Generate curl commands that delete XML API rules for given IPs.
     """
 
-    cmd_command = ""
+    cmd = ""
 
     for ip in ip_list:
         rule_name = f"FireBot-{ip}"
-        url = f"{host}/restapi/v11.0/Policies/SecurityRules?location=vsys&vsys={vsys}&name={rule_name}"
 
-        cmd_command += "curl -k -X DELETE \\\n"
-        cmd_command += "+  '" + url + "' \\\n"
-        cmd_command += "+  -H 'X-PAN-KEY: " + api_key + "' \\\n"
-        cmd_command += "+  -H 'Content-Type: application/json'\n\n"
+        xpath = (
+            f"/config/devices/entry/vsys/entry[@name='{vsys}']"
+            f"/rulebase/security/rules/entry[@name='{rule_name}']"
+        )
 
-    return cmd_command
+        cmd += (
+            "curl -k -X POST \\\n"
+            f"  '{host}/api/?key={api_key}&type=config&action=delete&xpath={xpath}'\n\n"
+        )
+
+    # Add COMMIT
+    cmd += (
+        "# COMMIT CHANGES\n"
+        "curl -k -X POST \\\n"
+        f"  '{host}/api/?key={api_key}&type=commit&cmd=<commit></commit>'\n"
+    )
+
+    return cmd
 
 
 def db_mark_block_ip_addresses(ip_list: list):
